26e9fd2c30405de051404c053156eb2f4a7a754c
Tok <tony.g.kay@gmail.com>
2019-02-20 17:46:25 -0700

    Added an ansible.cfg to course ansible_engine_foundations

--- grade_wrapper.sh
+++ grade_wrapper.sh
@@ -10,6 +10,17 @@ FTL_REPO_HOME=/opt/ftl-repo-clone/
 COURSE_NAME=ansible_engine_foundations
 LAB_NUMBER=03_01
 
+#
+# Setup Ansible 
+# 
+
+export FILE=${FTL_REPO_HOME}/courses/${COURSE_NAME}/ansible.cfg
+if [ -f $FILE ]; then
+   export ANSIBLE_CONFIG=${FILE}
+fi
+
+echo ${ANSIBLE_CONFIG}
+
 source ${VIRTUALENV}/bin/activate
 ansible --version
 ansible-playbook grade_course.yml     \
