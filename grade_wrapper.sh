8cdf799a032e0fdf1800b52d35ba746a8179f880
Tok <tony.g.kay@gmail.com>
2019-02-20 17:20:34 -0700

    Fixes to lab grading role

--- grade_wrapper.sh
+++ grade_wrapper.sh
@@ -8,11 +8,11 @@
 VIRTUALENV=/opt/virtualenvs/venv-grading
 FTL_REPO_HOME=/opt/ftl-repo-clone/
 COURSE_NAME=ansible_engine_foundations
-LAB=03_01
+LAB_NUMBER=03_01
 
 source ${VIRTUALENV}/bin/activate
 ansible --version
 ansible-playbook grade_course.yml     \
   -e course_name=${COURSE_NAME}       \
-  -e 
+  -e lab_number=${LAB_NUMBER}
 
