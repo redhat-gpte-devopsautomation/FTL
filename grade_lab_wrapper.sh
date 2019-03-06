0411a4ec04b53da35c20e80938b5422360a6a370
Tok <tony.g.kay@gmail.com>
2019-03-06 14:22:31 -0700

    Fixes new grading script

--- grade_lab_wrapper.sh
+++ grade_lab_wrapper.sh
@@ -35,37 +35,6 @@ ansible-playbook grade_lab.yml        \
   -e @vars/global_vars.yml            \
   -e @test-data/sample-vars.yml
 
-
-#  -e course_name=${COURSE_NAME}       \
-#  -e lab_number=${LAB_NUMBER} -vv   # > /tmp/opentlc_grader_log 2>&1
-
-# Output the error report
-
-cat /tmp/error_report.txt
-
-
-
-#
-# Setup Ansible 
-# 
-
-# Make sure ansible finds the correct roles if it loads a new ansible.cfg
-
-export ANSIBLE_ROLES_PATH=${FTL_REPO_HOME}/roles:${FTL_REPO_HOME}/roles-ftl-runtime
-export FILE=${FTL_REPO_HOME}/courses/${COURSE_NAME}/ansible.cfg
-
-if [ -f $FILE ]; then
-   export ANSIBLE_CONFIG=${FILE}
-fi
-
-source ${VIRTUALENV}/bin/activate
-
-ansible-playbook grade_lab.yml        \
-  -e @vars/global_vars.yml            \
-  -e @test-data/sample-vars.yml $@   # > /tmp/opentlc_grader_log 2>&1
-#  -e course_name=${COURSE_NAME}       \
-#  -e lab_number=${LAB_NUMBER} -vv   # > /tmp/opentlc_grader_log 2>&1
-
 # Output the error report
 
 cat /tmp/error_report.txt
