201674f2f955c6ca8f53074a5a69597d1434c91c
Tok <tony.g.kay@gmail.com>
2019-04-30 15:56:11 -0600

    More utility development and tooling in devel

--- devel/push-to-test-lab.sh
+++ devel/push-to-test-lab.sh
@@ -3,19 +3,21 @@
 # Sample script pushes a branch to a remote grader host
 
 # AWS Creds and github pull secret 
-SECRET_VARS=~/secret.yml
+SECRET_VARS=${SECRET_VARS:-~/secret.yml}
 
 # Developer supplied inventory for grader_host(s)
 # Examples here: 
 
-TEST_INVENTORY=
+TEST_INVENTORY=./devel/inventories/test-inventory
+TEST_INVENTORY=./devel/inventories/test-inventory
+
 # Which branch to push - defaults to master
 BRANCH=master
 
-ansible-playbook devel/setup-grading-host.yml             \
-  -i ${TEST_INVENTORY:-master}                            \
-  -e ${SECRET_VARS}                                       \
-  -e @devel/test-data-and-inventories/sample-vars.yml     \  
+ansible-playbook devel/setup-grading-host.yml     \
+  -i ${TEST_INVENTORY:-master}                    \
+  -e @${TEST_VARS:-./devel/sample-vars.yml}       \
+  -e @${SECRET_VARS}                               \
   -e ${BRANCH:-master}                            \
-  -b 
+  -b
 
