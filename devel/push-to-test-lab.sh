6ae3e71c1c93ac9003ed4a614308d8d7bf7d9597
Tok <tony.g.kay@gmail.com>
2019-05-01 14:35:31 -0600

    Setup sensible defaults for push to-test-lab

--- devel/push-to-test-lab.sh
+++ devel/push-to-test-lab.sh
@@ -1,23 +1,29 @@
 #!/usr/bin/env bash
 
+# Tony Kay, tok@redhat.com
+
 # Sample script pushes a branch to a remote grader host
+# 1) speeds up testing without requiring a new deploy
+
+
+# Developer supplied inventory for grader_host(s)
+
+TEST_INVENTORY=${TEST_INVENTORY:-./devel/inventories/test-inventory}
+
+# Developr supplied dummy vars see: `./devel/sample-vars.yml`
+
+TEST_VARS=${TEST_VARS:-./devel/sample-vars.yml}
 
 # AWS Creds and github pull secret 
 SECRET_VARS=${SECRET_VARS:-~/secret.yml}
 
-# Developer supplied inventory for grader_host(s)
-# Examples here: 
-
-TEST_INVENTORY=./devel/inventories/test-inventory
-TEST_INVENTORY=./devel/inventories/test-inventory
-
 # Which branch to push - defaults to master
-BRANCH=master
+BRANCH=${BRANCH:-master}
+
 
 ansible-playbook devel/setup-grading-host.yml     \
-  -i ${TEST_INVENTORY:-master}                    \
-  -e @${TEST_VARS:-./devel/sample-vars.yml}       \
-  -e @${SECRET_VARS}                               \
-  -e ${BRANCH:-master}                            \
+  -i ${TEST_INVENTORY}   \
+  -e @${TEST_VARS}       \
+  -e @${SECRET_VARS}     \
+  -e ${BRANCH}           \
   -b
-
